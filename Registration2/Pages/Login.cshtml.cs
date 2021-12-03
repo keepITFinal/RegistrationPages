da10cf9649a53452fa8b2158f36ff7c33edcae03
Naked-Snake04 <joKer041200saV@gmail.com>
2021-12-03 08:28:31 +0300

    Added registration

---
+++ Registration2/Pages/Login.cshtml.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Registration2.ViewModels;
+
+namespace Registration2.Pages
+{
+    public class LoginModel : PageModel
+    {
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        [BindProperty]
+        public Login Model { get; set; }
+
+        public LoginModel(SignInManager<IdentityUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returtnUrl = null)
+        {
+            if (ModelState.IsValid)
+            {
+                var identityResult = await signInManager.PasswordSignInAsync(Model.Email, Model.Password, Model.RememberMe, false);
+                if (identityResult.Succeeded)
+                {
+                    if (returtnUrl == null || returtnUrl == "/")
+                    {
+                        return RedirectToPage("Index");
+                    }
+                    else
+                    {
+                        return RedirectToPage(returtnUrl);
+                    }
+                }
+
+                ModelState.AddModelError("", "Username or Password incorrect");
+            }
+            return Page();
+        }
+    }
+}
---
+++ Registration2/ViewModels/Register.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Registration2.ViewModels
+{
+    public class Register
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage ="Пароли не совпадают")]
+        public string ConfirmPassword { get; set; }
+    }
+}
